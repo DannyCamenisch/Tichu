d347d9cd3d6b2bc83da950add05409a46aefdb1b
Danny Camenisch <dcamenisch@student.ethz.ch>
2021-03-24 19:24:26 +0100

    added serialVersionUID for CardSetException

--- ch/dcamenisch/tichu/Card/CardSetException.java
+++ ch/dcamenisch/tichu/Card/CardSetException.java
@@ -1,6 +1,8 @@
 package ch.dcamenisch.tichu.Card;
 
 public class CardSetException extends Exception {
+	private static final long serialVersionUID = -9177598784102959695L;
+
 	public CardSetException(String msg) {
 		super(msg);
 	}
