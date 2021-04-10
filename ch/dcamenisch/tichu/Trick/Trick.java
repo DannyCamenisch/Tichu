50d787362dcb4d00609c41e2a07266cdcb7a2577
Danny Camenisch <dcamenisch@student.ethz.ch>
2021-04-10 17:12:06 +0200

    added trick

--- ch/dcamenisch/tichu/Trick/Trick.java
+++ ch/dcamenisch/tichu/Trick/Trick.java
@@ -1,8 +1,27 @@
 package ch.dcamenisch.tichu.Trick;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class Trick {
+    List<CardSet> trick;
     
+    public Trick() {
+        trick = new LinkedList<CardSet>();
+    }
 
+    public void addCards(CardSet cs) {
+        trick.add(cs);
+    }
 
-    
+    public int getPoints() {
+        int counter = 0;
+        
+        for(CardSet cs : trick) {
+            counter += cs.getPoints();
+        }
+
+        return counter;
+    }
+
 }
